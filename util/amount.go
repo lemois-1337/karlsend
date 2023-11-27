2c6994a24a2c9b4ff020cce28fbc7a701d2e0c92
Lemois 1337 <lemois1337@protonmail.com>
2023-11-27 20:56:43 +0100

    Replaced KAS with KLS for cleanup commit series in util/

--- util/amount.go
+++ util/amount.go
@@ -21,33 +21,33 @@ type AmountUnit int
 // These constants define various units used when describing a kaspa
 // monetary amount.
 const (
-	AmountMegaKAS  AmountUnit = 6
-	AmountKiloKAS  AmountUnit = 3
-	AmountKAS      AmountUnit = 0
-	AmountMilliKAS AmountUnit = -3
-	AmountMicroKAS AmountUnit = -6
+	AmountMegaKLS  AmountUnit = 6
+	AmountKiloKLS  AmountUnit = 3
+	AmountKLS      AmountUnit = 0
+	AmountMilliKLS AmountUnit = -3
+	AmountMicroKLS AmountUnit = -6
 	AmountSompi    AmountUnit = -8
 )
 
 // String returns the unit as a string. For recognized units, the SI
 // prefix is used, or "Sompi" for the base unit. For all unrecognized
-// units, "1eN KAS" is returned, where N is the AmountUnit.
+// units, "1eN KLS" is returned, where N is the AmountUnit.
 func (u AmountUnit) String() string {
 	switch u {
-	case AmountMegaKAS:
-		return "MKAS"
-	case AmountKiloKAS:
-		return "kKAS"
-	case AmountKAS:
-		return "KAS"
-	case AmountMilliKAS:
-		return "mKAS"
-	case AmountMicroKAS:
-		return "μKAS"
+	case AmountMegaKLS:
+		return "MKLS"
+	case AmountKiloKLS:
+		return "kKLS"
+	case AmountKLS:
+		return "KLS"
+	case AmountMilliKLS:
+		return "mKLS"
+	case AmountMicroKLS:
+		return "μKLS"
 	case AmountSompi:
 		return "Sompi"
 	default:
-		return "1e" + strconv.FormatInt(int64(u), 10) + " KAS"
+		return "1e" + strconv.FormatInt(int64(u), 10) + " KLS"
 	}
 }
 
@@ -71,10 +71,10 @@ func round(f float64) Amount {
 // does not check that the amount is within the total amount of kaspa
 // producible as f may not refer to an amount at a single moment in time.
 //
-// NewAmount is for specifically for converting KAS to Sompi.
+// NewAmount is for specifically for converting KLS to Sompi.
 // For creating a new Amount with an int64 value which denotes a quantity of Sompi,
 // do a simple type conversion from type int64 to Amount.
-// TODO: Refactor NewAmount. When amounts are more than 1e9 KAS, the precision
+// TODO: Refactor NewAmount. When amounts are more than 1e9 KLS, the precision
 // can be higher than one sompi (1e9 and 1e9+1e-8 will result as the same number)
 func NewAmount(f float64) (Amount, error) {
 	// The amount is only considered invalid if it cannot be represented
@@ -97,9 +97,9 @@ func (a Amount) ToUnit(u AmountUnit) float64 {
 	return float64(a) / math.Pow10(int(u+8))
 }
 
-// ToKAS is the equivalent of calling ToUnit with AmountKAS.
-func (a Amount) ToKAS() float64 {
-	return a.ToUnit(AmountKAS)
+// ToKLS is the equivalent of calling ToUnit with AmountKLS.
+func (a Amount) ToKLS() float64 {
+	return a.ToUnit(AmountKLS)
 }
 
 // Format formats a monetary amount counted in kaspa base units as a
@@ -111,9 +111,9 @@ func (a Amount) Format(u AmountUnit) string {
 	return strconv.FormatFloat(a.ToUnit(u), 'f', -int(u+8), 64) + units
 }
 
-// String is the equivalent of calling Format with AmountKAS.
+// String is the equivalent of calling Format with AmountKLS.
 func (a Amount) String() string {
-	return a.Format(AmountKAS)
+	return a.Format(AmountKLS)
 }
 
 // MulF64 multiplies an Amount by a floating point value. While this is not
