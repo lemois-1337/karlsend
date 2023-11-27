812c701d41a4cf5f76883021d5ca08a3b788d978
Lemois 1337 <lemois1337@protonmail.com>
2023-11-27 21:09:15 +0100

    Replaced KAS with KLS for cleanup commit series in stability-tests/
    The tests itself will be fixed in a later commit series

--- stability-tests/rpc-idle-clients/run/run.sh
+++ stability-tests/rpc-idle-clients/run/run.sh
@@ -3,11 +3,11 @@ rm -rf /tmp/karlsend-temp
 
 NUM_CLIENTS=128
 karlsend --devnet --appdir=/tmp/karlsend-temp --profile=6061 --rpcmaxwebsockets=$NUM_CLIENTS &
-KASPAD_PID=$!
-KASPAD_KILLED=0
+KALRSEND_PID=$!
+KALRSEND_KILLED=0
 function killKarlsendIfNotKilled() {
-  if [ $KASPAD_KILLED -eq 0 ]; then
-    kill $KASPAD_PID
+  if [ $KALRSEND_KILLED -eq 0 ]; then
+    kill $KALRSEND_PID
   fi
 }
 trap "killKarlsendIfNotKilled" EXIT
@@ -17,16 +17,16 @@ sleep 1
 rpc-idle-clients --devnet --profile=7000 -n=$NUM_CLIENTS
 TEST_EXIT_CODE=$?
 
-kill $KASPAD_PID
+kill $KALRSEND_PID
 
-wait $KASPAD_PID
-KASPAD_EXIT_CODE=$?
-KASPAD_KILLED=1
+wait $KALRSEND_PID
+KALRSEND_EXIT_CODE=$?
+KALRSEND_KILLED=1
 
 echo "Exit code: $TEST_EXIT_CODE"
-echo "Karlsend exit code: $KASPAD_EXIT_CODE"
+echo "Karlsend exit code: $KALRSEND_EXIT_CODE"
 
-if [ $TEST_EXIT_CODE -eq 0 ] && [ $KASPAD_EXIT_CODE -eq 0 ]; then
+if [ $TEST_EXIT_CODE -eq 0 ] && [ $KALRSEND_EXIT_CODE -eq 0 ]; then
   echo "rpc-idle-clients test: PASSED"
   exit 0
 fi
